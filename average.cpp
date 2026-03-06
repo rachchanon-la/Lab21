75e34858c12102cc028eceeef40d0fd0627349a0
rachchanon-la <rachchanon_la@cmu.ac.th>
2026-03-06 13:01:04 +0700

    Update average.cpp

--- average.cpp
+++ average.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
 
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        cout << "Please input numbers to find average." << endl;
+        return 0;
+    }
+
+    int sum = 0;
+
+    for (int i = 1; i < argc; i++)
+    {
+        sum += atof(argv[i]);
+    }
+
+    int avg = sum / (argc - 1);
+
+    cout << "---------------------------------" << endl;
+    cout << "Average of " << argc - 1 << " numbers = "
+         << fixed << setprecision(3) << avg << endl;
+    cout << "---------------------------------" << endl;
+
+    return 0;
+}
